b633cafcd05eda5bed4a8df3ffc735248194ec45
boochix <179311815+boochix@users.noreply.github.com>
2025-07-05 21:29:46 +0530

    Update Solver.c
    dawg i dunno what im doing

--- Solver.c
+++ Solver.c
@@ -5,7 +5,6 @@
 #include <math.h>
 #include "API.h"
 
-
 struct cell_status {
     bool top_wall;
     bool bottom_wall;
@@ -23,15 +22,14 @@ struct position {
     int cols;
 };
 
-
 void log_msg(char* text) {
     fprintf(stderr, "%s", text);
     fflush(stderr);
 }
 
-void init_cell(struct cell_status **cell_stat, struct position pos) {
-    for(int i = 0; i < pos.rows; i++)
-        for(int j = 0; j < pos.cols; j++) {
+void init_cell(struct cell_status **cell_stat, struct position *pos) {
+    for(int i = 0; i < pos->rows; i++)
+        for(int j = 0; j < pos->cols; j++) {
             cell_stat[i][j].explored = false;
             cell_stat[i][j].top_wall = false;
             cell_stat[i][j].bottom_wall = false;
@@ -41,8 +39,8 @@ void init_cell(struct cell_status **cell_stat, struct position pos) {
         }
 }
 
-void init_cell_score(struct cell_status **cell_stat, struct position pos) {
-    int r2 = pos.rows / 2, c2 = pos.cols / 2;
+void init_cell_score(struct cell_status **cell_stat, struct position *pos) {
+    int r2 = pos->rows / 2, c2 = pos->cols / 2;
     for(int i = 0; i < r2; i++)
         for(int j = 0; j < c2; j++) {
             cell_stat[i + r2][j + c2].score = i + j;
@@ -52,61 +50,57 @@ void init_cell_score(struct cell_status **cell_stat, struct position pos) {
         }
 }
 
-void init_tester(struct cell_status **cell_stat, struct position pos) {
+void init_tester(struct cell_status **cell_stat, struct position *pos) {
     char score[4];
     log_msg("PES University IEEE RAS Maze Solving Algorithm\n");
     log_msg("FOR TESTING PURPOSES ONLY!\n");
     log_msg("Version: 0.3\n");
     log_msg("Type: unknown\n");
-    log_msg("Date: 05-07-2025\n");
-    log_msg("\n");
+    log_msg("Date: 05-07-2025\n\n");
 
-    for(int i = 0; i < pos.rows; i++) {
-        for(int j = 0; j < pos.cols; j++) {
+    for(int i = 0; i < pos->rows; i++) {
+        for(int j = 0; j < pos->cols; j++) {
             sprintf(score, "%d", cell_stat[i][j].score);
             API_setText(i, j, score);
-            //log_msg(score);
-            //log_msg("\t");
         }
-        //log_msg("\n");
     }
 }
 
-void set_wall(struct cell_status **cell_stat, struct position pos) {
-    if(cell_stat[pos.x][pos.y].top_wall)
-        API_setWall(pos.x, pos.y, 'n');
-    if(cell_stat[pos.x][pos.y].bottom_wall)
-        API_setWall(pos.x, pos.y, 's');
-    if(cell_stat[pos.x][pos.y].left_wall)
-        API_setWall(pos.x, pos.y, 'w');
-    if(cell_stat[pos.x][pos.y].right_wall)
-        API_setWall(pos.x, pos.y, 'e');
+void set_wall(struct cell_status **cell_stat, struct position *pos) {
+    if(cell_stat[pos->x][pos->y].top_wall)
+        API_setWall(pos->x, pos->y, 'n');
+    if(cell_stat[pos->x][pos->y].bottom_wall)
+        API_setWall(pos->x, pos->y, 's');
+    if(cell_stat[pos->x][pos->y].left_wall)
+        API_setWall(pos->x, pos->y, 'w');
+    if(cell_stat[pos->x][pos->y].right_wall)
+        API_setWall(pos->x, pos->y, 'e');
 }
 
-void check_wall(struct cell_status **cell_stat, struct position pos) {
-    API_setColor(pos.x, pos.y, 'y');
+void check_wall(struct cell_status **cell_stat, struct position *pos) {
+    API_setColor(pos->x, pos->y, 'y');
 
-    if(cell_stat[pos.x][pos.y].explored == false){
-        switch(pos.rot) {
+    if(cell_stat[pos->x][pos->y].explored == false){
+        switch(pos->rot) {
             case 0:
-                cell_stat[pos.x][pos.y].top_wall = API_wallFront();
-                cell_stat[pos.x][pos.y].left_wall = API_wallLeft();
-                cell_stat[pos.x][pos.y].right_wall = API_wallRight();
+                cell_stat[pos->x][pos->y].top_wall = API_wallFront();
+                cell_stat[pos->x][pos->y].left_wall = API_wallLeft();
+                cell_stat[pos->x][pos->y].right_wall = API_wallRight();
                 break;
             case 1:
-                cell_stat[pos.x][pos.y].top_wall = API_wallRight();
-                cell_stat[pos.x][pos.y].bottom_wall = API_wallLeft();
-                cell_stat[pos.x][pos.y].left_wall = API_wallFront();
+                cell_stat[pos->x][pos->y].top_wall = API_wallRight();
+                cell_stat[pos->x][pos->y].bottom_wall = API_wallLeft();
+                cell_stat[pos->x][pos->y].left_wall = API_wallFront();
                 break;
             case 2:
-                cell_stat[pos.x][pos.y].bottom_wall = API_wallFront();
-                cell_stat[pos.x][pos.y].left_wall = API_wallRight();
-                cell_stat[pos.x][pos.y].right_wall = API_wallLeft();
+                cell_stat[pos->x][pos->y].bottom_wall = API_wallFront();
+                cell_stat[pos->x][pos->y].left_wall = API_wallRight();
+                cell_stat[pos->x][pos->y].right_wall = API_wallLeft();
                 break;
             case 3:
-                cell_stat[pos.x][pos.y].top_wall = API_wallLeft();
-                cell_stat[pos.x][pos.y].bottom_wall = API_wallRight();
-                cell_stat[pos.x][pos.y].right_wall = API_wallFront();
+                cell_stat[pos->x][pos->y].top_wall = API_wallLeft();
+                cell_stat[pos->x][pos->y].bottom_wall = API_wallRight();
+                cell_stat[pos->x][pos->y].right_wall = API_wallFront();
                 break;
             default:
                 log_msg("Error checking walls");
@@ -114,83 +108,99 @@ void check_wall(struct cell_status **cell_stat, struct position pos) {
         }
         set_wall(cell_stat, pos);
     }
-    cell_stat[pos.x][pos.y].explored = true;
+    cell_stat[pos->x][pos->y].explored = true;
 }
 
-void get_move(struct cell_status **cell_stat, struct position pos) 
-{
-    int score_n = pos.rows*pos.cols, score_s = pos.rows*pos.cols, score_e = pos.rows*pos.cols, score_w = pos.rows*pos.cols;
-    if(pos.y < 15 && !cell_stat[pos.x][pos.y].top_wall)
-        score_n = cell_stat[pos.x][pos.y+1].score ;
-    if(pos.y > 0 && !cell_stat[pos.x][pos.y].bottom_wall)
-        score_s = cell_stat[pos.x][pos.y-1].score;
-    if(pos.x > 0 && !cell_stat[pos.x][pos.y].right_wall)
-        score_e = cell_stat[pos.x-1][pos.y].score;
-    if(pos.x < 15 && !cell_stat[pos.x][pos.y].left_wall)
-        score_w = cell_stat[pos.x+1][pos.y].score; 
-    switch(pos.rot){
-        case 0: 
+void get_move(struct cell_status **cell_stat, struct position *pos) {
+    char score[4];
+    int score_n = pos->rows * pos->cols;
+    int score_s = pos->rows * pos->cols;
+    int score_e = pos->rows * pos->cols;
+    int score_w = pos->rows * pos->cols;
+
+    if(pos->y < pos->cols - 1 && !cell_stat[pos->x][pos->y].top_wall)
+        score_n = cell_stat[pos->x][pos->y+1].score;
+    if(pos->y > 0 && !cell_stat[pos->x][pos->y].bottom_wall)
+        score_s = cell_stat[pos->x][pos->y-1].score;
+    if(pos->x > 0 && !cell_stat[pos->x][pos->y].right_wall)
+        score_e = cell_stat[pos->x-1][pos->y].score;
+    if(pos->x < pos->rows - 1 && !cell_stat[pos->x][pos->y].left_wall)
+        score_w = cell_stat[pos->x+1][pos->y].score;
+
+    sprintf(score, "%d\t%d\t%d\t%d\n", score_e, score_n, score_s, score_w);
+    log_msg(score);
+
+    switch(pos->rot){
+        case 0:
             if(score_n <= score_e && score_n <= score_s && score_n <= score_w){
                 API_moveForward();
-                pos.y++;
+                pos->y++;
             }
             else if(score_w <= score_n && score_w <= score_s && score_w <= score_e){
                 API_turnLeft();
+                pos->rot = 1;
                 API_moveForward();
-                pos.x--;
+                pos->x--;
             }
             else if(score_e <= score_n && score_e <= score_w && score_e <= score_s){
                 API_turnRight();
+                pos->rot = 3;
                 API_moveForward();
-                pos.x++;
+                pos->x++;
             }
             break;
         case 1:
             if(score_w <= score_e && score_w <= score_s && score_w <= score_n){
                 API_moveForward();
-                pos.x--;
+                pos->x--;
             }
             else if(score_n <= score_w && score_n <= score_s && score_n <= score_e){
                 API_turnRight();
+                pos->rot = 0;
                 API_moveForward();
-                pos.y++;
+                pos->y++;
             }
             else if(score_s <= score_n && score_s <= score_w && score_s <= score_e){
                 API_turnLeft();
+                pos->rot = 2;
                 API_moveForward();
-                pos.y--;
+                pos->y--;
             }
             break;
         case 2:
             if(score_s <= score_e && score_s <= score_n && score_s <= score_w){
                 API_moveForward();
-                pos.y--;
+                pos->y--;
             }
             else if(score_w <= score_n && score_w <= score_s && score_w <= score_e){
                 API_turnRight();
+                pos->rot = 1;
                 API_moveForward();
-                pos.x--;
+                pos->x--;
             }
             else if(score_e <= score_n && score_e <= score_w && score_e <= score_s){
                 API_turnLeft();
+                pos->rot = 3;
                 API_moveForward();
-                pos.x++;
+                pos->x++;
             }
             break;
         case 3:
             if(score_e <= score_n && score_e <= score_s && score_e <= score_w){
                 API_moveForward();
-                pos.x++;
+                pos->x++;
             }
             else if(score_s <= score_n && score_s <= score_w && score_s <= score_e){
                 API_turnRight();
+                pos->rot = 2;
                 API_moveForward();
-                pos.y--;
+                pos->y--;
             }
             else if(score_n <= score_e && score_n <= score_w && score_n <= score_s){
                 API_turnLeft();
+                pos->rot = 0;
                 API_moveForward();
-                pos.y++;
+                pos->y++;
             }
             break;
         default:
@@ -221,16 +231,14 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    init_cell(cell_stat, pos);
-    init_cell_score(cell_stat, pos);
-    init_tester(cell_stat, pos);
+    init_cell(cell_stat, &pos);
+    init_cell_score(cell_stat, &pos);
+    init_tester(cell_stat, &pos);
 
-    while (true) {
-        check_wall(cell_stat, pos);
-        get_move(cell_stat, pos);
+    for(int l = 0; l < 300; l++) {
+        check_wall(cell_stat, &pos);
+        get_move(cell_stat, &pos);
     }
-
-
 
     for (i = 0; i < pos.rows; i++)
         free(cell_stat[i]);
